4062de7d5042d0d567eca15408b2140c1f2f43eb
Scott Brissenden <sbrissenden@gmail.com>
2016-06-09 16:55:12 -0400

    Feature: Implement app launcher feature

--- app/src/main/java/com/udacity/scottbrissenden/myportfolioapp/MainActivity.java
+++ app/src/main/java/com/udacity/scottbrissenden/myportfolioapp/MainActivity.java
@@ -1,7 +1,11 @@
 package com.udacity.scottbrissenden.myportfolioapp;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /** Called when the user touches the button */
+    public void sendMessage(View view) {
+        // Display a snappy message when the user pushes a button
+        Context context = getApplicationContext();
+        Button button = (Button)view;
+
+        String text = "This button will launch my \"" + button.getText().toString() + "\" app!";
+        int duration = Toast.LENGTH_SHORT;
+        Toast.makeText(context, text, duration).show();
     }
 }
